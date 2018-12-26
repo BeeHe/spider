267d239bade4ea4e62b74e6a3f2f351da9c0715c
HeBee <hebee0954@gmail.com>
2018-12-26 11:49:22 +0800

    modfied send_email now use ssl to send email

--- send_email.py
+++ send_email.py
@@ -7,7 +7,7 @@ import smtplib
 
 # email config
 MAIL_SERVER = os.environ.get("MAIL_SERVER") or "smtp.163.com"
-MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+MAIL_PORT = int(os.environ.get("MAIL_PORT") or 465)
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 ADMINS = ['hebee0954@gmail.com', "609926981@qq.com"]
@@ -25,8 +25,8 @@ def send_email(filename):
         message['Subject'] = filename
 
         try:
-            print(MAIL_PORT, MAIL_USERNAME)
-            smtp_obj = smtplib.SMTP()
+            print(MAIL_PORT, MAIL_USERNAME, message['To'])
+            smtp_obj = smtplib.SMTP_SSL()
             smtp_obj.connect(MAIL_SERVER, MAIL_PORT)
             smtp_obj.login(MAIL_USERNAME, MAIL_PASSWORD)
             smtp_obj.sendmail(MAIL_USERNAME, ADMINS, message.as_string())
